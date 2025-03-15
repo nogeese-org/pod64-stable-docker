83d26000e2dcf2948c4602767e3d327bea5cbe1f
Nogeese <leon8326nogeese@outlook.com>
2025-03-15 09:35:12 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+$SOURCEPOD64=$(pwd)
+
+cd $SOURCEPOD64/build/essential/script.sh.d/
+echo "arch=pod64-docker" >> ~/.bashrc
+source ~/.bashrc
+echo "curl https://raw.githubusercontent.com/nogeese-org/mirror/main/nogeese/os/$arch/setup.sh | sudo bash" >> pacman.sh
+echo "curl https://raw.githubusercontent.com/nogeese-org/mirror/main/bit/os/$arch/setup.sh | sudo bash" >> pacman.sh
+bash pacman.sh
+cd $SOURCEPOD64/build
+mkdir appsrc
+cd appsrc
+curl -sSL https://raw.githubusercontent.com/nogeese-org/InfoKit/main/installer/install.sh | sudo bash
+cd ../sys
+git clone https://github.com/nogeese-org/stable.git ~/master
+cd master
+bash architectures/pod64-docker.sh
+bash scripts/appsetup.sh
+cd ../../appsrc
+git clone https://github.com/nogeese-org/podutils-docker.git ~/podutils
+cd podutils
+bash scripts/flush.sh
+clear
+echo "This message was automaticly sent."
+echo "The system is ready to use."
+exit 1
